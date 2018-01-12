5faef61691b2539d26f2bfe527d8da85ba18852d
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-01-12 13:54:31 +0000

    cucumber init

---
+++ native1/cucumber-appium/features/support/env.rb
@@ -0,0 +1,15 @@
+require 'appium_lib'
+require 'cucumber'
+require_relative '../../lib/budget_app'
+
+def opts
+  {
+    caps: {
+      platformName: "Android",
+      deviceName: "emulator-5554",
+      app: "/Users/tech-a04/engineering/sparta-mobile-testing/native1/app/budgetwatch.apk"
+    }
+  }
+end
+
+Appium::Driver.new(opts, true)
--- native1/rspec_appium/spec/budget_app_example_spec.rb
+++ native1/rspec_appium/spec/budget_app_example_spec.rb
@@ -17,14 +17,23 @@ describe 'Testing the budget app' do
   end
 
   it "Should have the budget option on the main landing page" do
-
+    find_elements(:id, 'protect.budgetwatch:id/menu')[0].displayed?
+    # expect(find_elements(:id, 'protect.budgetwatch:id/menu')[0].text).to eq 'Budgets'
+    # find_elements(:xpath, '//android.widget.LinearLayout[1]')[0].displayed?
   end
 
   it "Should access the budget page" do
-
+    scroll_to('Budgets').click
+    find_elements(:id, 'protect.budgetwatch:id/action_add')[0].displayed?
+    # find_elements(:xpath, '//android.widget.LinearLayout[1]')[0].click
   end
 
   it "Should be able to add a budget" do
+    find_element(:id, 'protect.budgetwatch:id/action_add').click
+    find_element(:id, 'protect.budgetwatch:id/budgetNameEdit').send_keys('test')
+    find_element(:id, 'protect.budgetwatch:id/valueEdit').send_keys('500')
+    find_element(:id, 'protect.budgetwatch:id/action_save').click
+    expect(find_elements(:id, 'protect.budgetwatch:id/budgetName')[0].text).to eq("test")
 
   end
 
